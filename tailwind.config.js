3982e30d32e5c775c1a779a447b0c52571163190
sootopia <shpark7502@naver.com>
2024-07-14 20:28:57 +0900

    Tailwind CSS 설치

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  important: true,
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
