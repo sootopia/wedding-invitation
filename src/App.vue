d839868a1cd7fb8928251ceea1f6ae778a40cab3
sootopia <shpark7502@naver.com>
2024-07-27 22:17:45 +0900

    갤러리 섹션 추가

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <HeroSection />
+    <InvitationSection />
+    <GallerySection />
   </div>
 </template>
 
 <script>
 import { ref, onMounted } from 'vue';
 import HeroSection from '@/components/sections/HeroSection.vue';
+import InvitationSection from '@/components/sections/InvitationSection.vue';
+import GallerySection from '@/components/sections/GallerySection.vue';
 
 export default {
   components: {
     HeroSection,
+    InvitationSection,
+    GallerySection,
   },
   setup() {},
 };
@@ -20,9 +26,9 @@ export default {
 #app {
   position: relative;
   max-width: 420px;
-  min-height: 6000px;
   margin-left: auto;
   margin-right: auto;
+  padding-bottom: 80px;
   background-color: $white;
 }
 </style>
