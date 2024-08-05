89a3aa7e2fcc0bb58af01b9202944d6256cdadc0
sootopia <shpark7502@naver.com>
2024-08-05 16:40:53 +0900

    vue-easy-lightbox 설치

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue';
 import '@/assets/css/index.css';
 import BaseButton from '@/components/BaseButton.vue';
 import { openAlert } from '@/plugins/alertPlugin';
+import VueEasyLightbox from 'vue-easy-lightbox';
 
 const app = createApp(App);
 app.component('BaseButton', BaseButton);
+app.use(VueEasyLightbox);
 app.config.globalProperties.$openAlert = openAlert;
 app.mount('#app');
